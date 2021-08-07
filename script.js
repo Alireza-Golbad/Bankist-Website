ee6bf4d923b035f6f03aa00f7159b25a479aed79
Alireza Golbad <63424607+Alireza-Golbad@users.noreply.github.com>
2021-08-07 20:39:42 +0430

    Modal window added

--- script.js
+++ script.js
@@ -2,7 +2,30 @@
 
 ///////////////////////////////////////
 // Modal window
-console.log(smile());
-function smile() {
-  return ':)';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn-close-modal');
+const btnOpenModal = document.querySelectorAll('.btn-show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
