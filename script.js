5ab0e698db1035f92c56244a87d52017ccdc2d1f
Alireza Golbad <63424607+Alireza-Golbad@users.noreply.github.com>
2021-08-21 21:00:39 +0430

    Scroll smooth

--- script.js
+++ script.js
@@ -1,31 +1,85 @@
-'use strict';
+"use strict";
 
 ///////////////////////////////////////
 // Modal window
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn-close-modal');
-const btnOpenModal = document.querySelectorAll('.btn-show-modal');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn-close-modal");
+const btnOpenModal = document.querySelectorAll(".btn-show-modal");
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
-for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', openModal);
-}
+btnOpenModal.forEach(btn => btn.addEventListener("click", openModal));
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
 });
+
+// Smooth Scrolling
+const btnScrollTo = document.querySelector(".btn-scroll-to");
+const section1 = document.querySelector("#section-1");
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+
+  // Scrooling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: "smooth",
+  // });
+
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+const header = document.querySelector(".header");
+const allSections = document.querySelectorAll(".section");
+
+// Creating and inserting Elements
+// document.getElementById("section-2").insertAdjacentHTML("beforeend", ...)
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+// message.textContent =
+//   "We use cookies for improved functionality and analytics.";
+
+message.innerHTML =
+  "We use cookies for improved functionality and analytics. <button class='btn btn-close-cookie'>Got it!</button>";
+header.append(message);
+// Delete elements
+document
+  .querySelector(".btn-close-cookie")
+  .addEventListener("click", function () {
+    message.remove();
+  });
+
+// Styles
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 85 + "px";
+
+// document.documentElement.style.setProperty("--color-primary", "orangered");
